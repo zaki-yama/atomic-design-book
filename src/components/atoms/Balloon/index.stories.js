e017332e596e3e12e80f5c294c65a0dbd5dedf48
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-05-16 02:01:04 +0900

    [p113] ラベル内容を表示するという関心を分離する

--- src/components/atoms/Balloon/index.stories.js
+++ src/components/atoms/Balloon/index.stories.js
@@ -1,4 +1,7 @@
 import React from "react";
 import Balloon from "./index";
 
-export default stories => stories.add("デフォルト", () => <Balloon />);
+export default stories =>
+  stories
+    .add("2文字ラベル", () => <Balloon>次へ</Balloon>)
+    .add("4文字ラベル", () => <Balloon>削除する</Balloon>);
