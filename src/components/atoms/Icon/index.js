d2b1ff4cfdd083e8db09945976f5b286d104c45e
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-05-22 19:09:43 +0900

    [p126] スタイル変更のロジックを分離する

--- src/components/atoms/Icon/index.js
+++ src/components/atoms/Icon/index.js
@@ -2,22 +2,36 @@ import React from "react";
 import styles from "./styles.css";
 import { defaultProps } from "recompose";
 
-export const TrashCanIcon = ({
+export const TrashCanIconPresenter = ({
   height = 20,
   width = 20,
-  className = "",
+  ...props
+}) => (
+  <img
+    src="/icons/trash-can.svg"
+    alt=""
+    height={height}
+    width={width}
+    {...props}
+  />
+);
+
+export const IconContainer = ({
+  presenter,
   onClick,
+  className = "",
   ...props
 }) => {
   if (onClick) className += ` ${styles.clickable}`;
+  return presenter({ onClick, className, ...props });
+};
+
+export const TrashCanIcon = props => {
   return (
-    <img
-      src="/icons/trash-can.svg"
-      alt=""
-      height={height}
-      width={width}
-      className={className}
-      onClick={onClick}
+    <IconContainer
+      presenter={presenterProps => (
+        <TrashCanIconPresenter {...presenterProps} />
+      )}
       {...props}
     />
   );
