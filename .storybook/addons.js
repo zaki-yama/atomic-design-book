fb295594797fb1e36ff371e092401122cf22c43e
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-06-03 22:38:16 +0900

    TypeScriptを導入

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,3 +1,2 @@
-import '@storybook/addon-actions/register';
-import '@storybook/addon-chapters/register';
-import '@storybook/addon-notes/register';
+import "@storybook/addon-actions/register";
+import "@storybook/addon-notes/register";
--- config/webpack.js
+++ config/webpack.js
@@ -1,68 +1,66 @@
-'use strict';
+"use strict";
 
-const webpack = require('webpack');
-const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require("webpack");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const build = require('./build');
+const build = require("./build");
 
 module.exports = merge => {
   const wpc = Object.assign(merge || {}, {
-
     output: Object.assign({}, build.js.output, {
-      libraryTarget: 'umd'
+      libraryTarget: "umd"
     }),
-
-    plugins: [
-      new ExtractTextPlugin('app.css')
-    ],
+    plugins: [new MiniCssExtractPlugin({ filename: "app.css" })],
 
     resolve: {
-      extensions: [ '*', '.js', '.jsx', '.css' ],
-      modules: [
-        'node_modules',
-        `${ build.projDir }/src`,
-      ],
+      extensions: ["*", ".ts", ".tsx", ".js", ".jsx", ".css"],
+      modules: ["node_modules", `${build.projDir}/src`]
     },
 
     module: {
-
-      rules: [{
-        test: /\.jsx?$/,
-        use: 'babel-loader'
-      }, {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
+      rules: [
+        {
+          test: /\.jsx?$/,
+          use: "babel-loader"
+        },
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader"
+        },
+        {
+          test: /\.css$/,
           use: [
+            MiniCssExtractPlugin.loader,
+            "css-modules-typescript-loader",
             {
-              loader: 'css-loader',
+              loader: "css-loader",
               options: {
                 sourceMap: true,
                 modules: true,
                 importLoaders: 1,
-                localIdentName: '[local]___[hash:base64:5]',
-              },
+                localIdentName: "[local]___[hash:base64:5]"
+              }
             },
             {
-              loader: 'postcss-loader',
+              loader: "postcss-loader",
               options: {
-                plugins: () => ([
-                  require("postcss-import")({path: ["node_modules", build.src]}),
-                  require("postcss-cssnext")(),
-                ]),
-              },
-            },
-          ],
-        }),
-      }],
-
-    },
-
+                plugins: () => [
+                  require("postcss-import")({
+                    path: ["node_modules", build.src]
+                  }),
+                  require("postcss-cssnext")()
+                ]
+              }
+            }
+          ]
+        }
+      ]
+    }
   });
 
   if (!build.production) {
-    wpc.devtool = '#source-map';
+    wpc.devtool = "#source-map";
   }
 
   return wpc;
